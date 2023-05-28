681e4e0379d020d6653928a0d6b57cdbe6a1cfb0
piomin <piotr.minkowski@gmail.com>
2023-05-29 00:28:27 +0200

    add testcontainers + hoverfly junit5

--- account-service/src/test/java/pl/piomin/services/account/AccountComponentTest.java
+++ account-service/src/test/java/pl/piomin/services/account/AccountComponentTest.java
@@ -1,26 +1,63 @@
 package pl.piomin.services.account;
 
-import org.junit.Assert;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 import pl.piomin.services.account.model.Account;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @ActiveProfiles({"test", "no-discovery"})
+@Testcontainers
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class AccountComponentTest {
 
     @Autowired
     TestRestTemplate restTemplate;
 
+    @Container
+    @ServiceConnection
+    static MongoDBContainer mongo = new MongoDBContainer("mongo:5.0");
+    static String id;
+
     @Test
-    public void testAdd() throws Exception {
+    @Order(1)
+    void testAdd() {
         Account account = new Account("1234567890", 5000, "1");
         account = restTemplate.postForObject("/", account, Account.class);
-        Assert.assertNotNull(account);
+        assertNotNull(account);
+        assertNotNull(account.getId());
+        assertEquals("1234567890", account.getNumber());
+        id = account.getId();
     }
 
+    @Test
+    @Order(2)
+    void testFindById() {
+        Account a = restTemplate.getForObject("/{id}", Account.class, id);
+        assertNotNull(a);
+        assertNotNull(a.getId());
+        assertEquals(id, a.getId());
+    }
+
+    @Test
+    @Order(2)
+    void testFindByCustomerId() {
+        Account[] a = restTemplate.getForObject("/customer/{id}", Account[].class, "1");
+        assertNotNull(a);
+        assertEquals(1, a.length);
+        assertNotNull(a[0].getId());
+    }
 }
--- customer-service/src/test/java/pl/piomin/services/customer/CustomerComponentTest.java
+++ customer-service/src/test/java/pl/piomin/services/customer/CustomerComponentTest.java
@@ -1,55 +1,82 @@
 package pl.piomin.services.customer;
 
-import static io.specto.hoverfly.junit.core.SimulationSource.dsl;
-import static io.specto.hoverfly.junit.dsl.HoverflyDsl.service;
-import static io.specto.hoverfly.junit.dsl.ResponseCreators.success;
-import static io.specto.hoverfly.junit.dsl.matchers.HoverflyMatchers.startsWith;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
-import java.util.concurrent.TimeUnit;
-
+import io.specto.hoverfly.junit.core.Hoverfly;
+import io.specto.hoverfly.junit5.HoverflyExtension;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
-
-import io.specto.hoverfly.junit.rule.HoverflyRule;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 import pl.piomin.services.customer.model.Customer;
 import pl.piomin.services.customer.model.CustomerType;
 
-//@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
-//@FixMethodOrder(MethodSorters.NAME_ASCENDING)
-//@ActiveProfiles({"test", "no-discovery"})
+import java.util.concurrent.TimeUnit;
+
+import static io.specto.hoverfly.junit.core.SimulationSource.dsl;
+import static io.specto.hoverfly.junit.dsl.HoverflyDsl.service;
+import static io.specto.hoverfly.junit.dsl.ResponseCreators.success;
+import static io.specto.hoverfly.junit.dsl.matchers.HoverflyMatchers.startsWith;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@ActiveProfiles({"test", "no-discovery"})
+@Testcontainers
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@ExtendWith(HoverflyExtension.class)
 public class CustomerComponentTest {
 
-//    @Autowired
+    @Autowired
     TestRestTemplate restTemplate;
 
-//    @ClassRule
-//    public static HoverflyRule hoverflyRule = HoverflyRule
-//            .inSimulationMode(dsl(
-//                    service("account-service:8080")
-//                            .andDelay(200, TimeUnit.MILLISECONDS).forAll()
-//                            .get(startsWith("/customer/"))
-//                            .willReturn(success("[{\"id\":\"1\",\"number\":\"1234567890\",\"balance\":5000}]", "application/json"))))
-//            .printSimulationData();
+    @Container
+    @ServiceConnection
+    static MongoDBContainer mongo = new MongoDBContainer("mongo:5.0");
+    static String id;
 
-//    @Test
+    @Test
+    @Order(1)
     public void testAdd() {
         Customer customer = new Customer("Test1", CustomerType.REGULAR);
         customer = restTemplate.postForObject("/", customer, Customer.class);
         assertNotNull(customer);
+        assertNotNull(customer.getId());
+        assertEquals("Test1", customer.getName());
+        id = customer.getId();
     }
 
-//    @Test
-    public void testFindWithAccounts() {
+    @Test
+    @Order(2)
+    void testFindById() {
+        Customer c = restTemplate.getForObject("/{id}", Customer.class, id);
+        assertNotNull(c);
+        assertNotNull(c.getId());
+        assertEquals(id, c.getId());
+    }
+
+    @Test
+    @Order(2)
+    public void testFindWithAccounts(Hoverfly hoverfly) {
+        hoverfly.simulate(dsl(
+                service("account-service")
+                        .andDelay(200, TimeUnit.MILLISECONDS).forAll()
+                        .get(startsWith("/customer/"))
+                        .willReturn(success("[{\"id\":\"1\",\"number\":\"1234567890\",\"balance\":5000}]", "application/json"))));
         Customer customer = new Customer("Test2", CustomerType.REGULAR);
-        customer = restTemplate.postForObject("/", customer, Customer.class);
-//		customer = restTemplate.getForObject("/withAccounts/{id}", Customer.class, customer.getId());
+//        customer = restTemplate.postForObject("/", customer, Customer.class);
+		customer = restTemplate.getForObject("/withAccounts/{id}", Customer.class, id);
         assertNotNull(customer);
-//		Assert.assertNotNull(customer.getAccounts());
-//		Assert.assertEquals(1, customer.getAccounts().size());
+		assertNotNull(customer.getAccounts());
+//		assertEquals(1, customer.getAccounts().size());
+//        assertEquals("1", customer.getAccounts().get(0).getId());
     }
 }
